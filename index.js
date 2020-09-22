21df1b3bcd30f021dfb831c9f6fa4b21180cba8c
Jon Jessee <tjessee7624@gmail.com>
2020-09-22 15:41:30 -0600

    Wrote switch statement that creates a badge depending on which license the user selected for their project.

--- index.js
+++ index.js
@@ -118,7 +118,41 @@ function getLicense(answers) {
 
 //Returned data is compiled into a markdown format to be written
 function generateReadme(answers, license) {
+    //Based on chosen license a badge is selected for the README
+    let badge ='';
+    switch(answers.license) {
+        case 'MIT':
+            badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+
+        case 'GNU Lesser General Public License v3.0':
+            badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](http://www.gnu.org/licenses/lgpl-3.0)';
+            break;
+
+        case 'Mozilla Public License 2.0':
+            badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+            break;
+
+        case 'GNU Affero General Public License v3.0':
+            badge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)';
+            break;
+
+        case 'The Unlicense':
+            badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+            break;
+
+        case 'Apache License 2.0':
+            badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+
+        case 'GNU General Public License v3.0':
+            badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)';
+            break;
+            
+    };
+
     return `#${answers.title}
+    ${badge}
 
     ##Table of contents
     [License](#License)
