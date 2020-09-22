6b720e720f454c476b0c188d4b9880dbf0ff2c80
Jon Jessee <tjessee7624@gmail.com>
2020-09-22 13:08:21 -0600

    Altered functiions to use async await. Axios calls now successful and readme file is generated with the users input data.

--- index.js
+++ index.js
@@ -8,105 +8,112 @@ const { clear } = require('console');
 //Promisify writeFile function for later use
 const writeReadmeAsync = util.promisify(fs.writeFile);
 
-// Function collects all answers from the user about their README via inquirer
-function getAnswers() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title'
-        },
-        {
-            type: 'list',
-            message: 'What license would you like to use for your project?',
-            choices: [
-                'MIT',
-                'GNU Lesser General Public License v3.0',
-                'Mozilla Public License 2.0',
-                'GNU Affero General Public License v3.0',
-                'The Unlicense',
-                'Apache License 2.0',
-                'GNU General Public License v3.0'
-            ],
-            name: 'license'
-        },
-        {
-            type: 'input',
-            message: 'Describe your project and its function.',
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: 'How does the user install your application?',
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: 'Enter some examples and show the expected output from your application',
-            name: 'usage'
-        },
-        {
-            type: 'input',
-            message: 'Deescribe to the user how they can contribute to your project.',
-            name: 'contributing'
-        },
-        {
-            type: 'input',
-            message: 'How can the user test your application once installed?',
-            name: 'tests'
-        },
-        {
-            type: 'input',
-            message: 'What is your full GitHub URL?',
-            name: 'github'
-        },
-        {
-            type: 'input',
-            message: 'What email address can users reach you at?',
-            name: 'email'
-        }
-    ]);
-};
+async function getAnswers() {
+    try {
+        //Inquirer prompts are all answered and stored
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title'
+            },
+            {
+                type: 'list',
+                message: 'What license would you like to use for your project?',
+                choices: [
+                    'MIT',
+                    'GNU Lesser General Public License v3.0',
+                    'Mozilla Public License 2.0',
+                    'GNU Affero General Public License v3.0',
+                    'The Unlicense',
+                    'Apache License 2.0',
+                    'GNU General Public License v3.0'
+                ],
+                name: 'license'
+            },
+            {
+                type: 'input',
+                message: 'Describe your project and its function.',
+                name: 'description'
+            },
+            {
+                type: 'input',
+                message: 'How does the user install your application?',
+                name: 'installation'
+            },
+            {
+                type: 'input',
+                message: 'Enter some examples and show the expected output from your application',
+                name: 'usage'
+            },
+            {
+                type: 'input',
+                message: 'Deescribe to the user how they can contribute to your project.',
+                name: 'contributing'
+            },
+            {
+                type: 'input',
+                message: 'How can the user test your application once installed?',
+                name: 'tests'
+            },
+            {
+                type: 'input',
+                message: 'What is your full GitHub URL?',
+                name: 'github'
+            },
+            {
+                type: 'input',
+                message: 'What email address can users reach you at?',
+                name: 'email'
+            }
+        ]);
 
 
-function getLicense(answers) {
-    let githubURL = '';
-    switch(answers.license) {
-        case 'MIT':
-            githubURL = 'https://api.github.com/licenses/mit';
-            break;
+        //The desired license is passed to the AXIOS module in order to return licensing info
+        let githubURL = '';
+        switch(answers.license) {
+            case 'MIT':
+                githubURL = 'https://api.github.com/licenses/mit';
+                break;
 
-        case 'GNU Lesser General Public License v3.0':
-            githubURL = 'https://api.github.com/licenses/lgpl-3.0';
-            break;
+            case 'GNU Lesser General Public License v3.0':
+                githubURL = 'https://api.github.com/licenses/lgpl-3.0';
+                break;
 
-        case 'Mozilla Public License 2.0':
-            githubURL = 'https://api.github.com/licenses/mpl-2.0';
-            break;
+            case 'Mozilla Public License 2.0':
+                githubURL = 'https://api.github.com/licenses/mpl-2.0';
+                break;
 
-        case 'GNU Affero General Public License v3.0':
-            githubURL = 'https://api.github.com/licenses/agpl-3.0';
-            break;
+            case 'GNU Affero General Public License v3.0':
+                githubURL = 'https://api.github.com/licenses/agpl-3.0';
+                break;
 
-        case 'The Unlicense':
-            githubURL = 'https://api.github.com/licenses/unlicense';
-            break;
+            case 'The Unlicense':
+                githubURL = 'https://api.github.com/licenses/unlicense';
+                break;
 
-        case 'Apache License 2.0':
-            githubURL = 'https://api.github.com/licenses/apache-2.0';
-            break;
+            case 'Apache License 2.0':
+                githubURL = 'https://api.github.com/licenses/apache-2.0';
+                break;
 
-        case 'GNU General Public License v3.0':
-            githubURL = 'https://api.github.com/licenses/gpl-3.0';
+            case 'GNU General Public License v3.0':
+                githubURL = 'https://api.github.com/licenses/gpl-3.0';
+  
+        };
 
-        return axios
-        .get(githubURL);  
+        const license = await axios.get(githubURL);
+
+        //Readme is genereated and written to the file
+        const text = await generateReadme(answers, license);
+        await writeReadmeAsync('READMEtest.md', text);
+    } catch(error) {
+        console.log(error)
     };
 };
 
 
-//generateReadme will take in the user data and format it to be written
-function generateReadme(answers, response) {
+//Returned data is compiled into a markdown format to be written
+function generateReadme(answers, license) {
     return `#${answers.title}
 
     ##Table of contents
@@ -119,7 +126,7 @@ function generateReadme(answers, response) {
     [Questions](#Questions)
 
     ###License
-    ${response.data.body}
+    ${license.data.body}
     
     ###Description
     ${answers.description}
@@ -143,34 +150,11 @@ function generateReadme(answers, response) {
 };
 
 
+//Execute the application
 getAnswers()
-.then((answers) => {
-    getLicense(answers);
-})
-.then((answers, response) => {
-    const readmeText = generateReadme(answers, response);
-    return writeReadmeAsync('reademetest.md', readmeText);
-})
 .then(() => {
-    console.log('Successfully written!');
+    console.log('Your README file was written successfully!')
 })
-.catch((err) => {
-    if(err) {
-        console.log(err);
-    };
+.catch((error) => {
+    console.log(error);
 });
-
-
-
-// //Data is collected and the README is written to its file
-// getAnswers()
-// .then((answers) => {
-//     const readmeText = generateReadme(answers);
-//     return writeReadmeAsync('readmetest.md', readmeText);
-// })
-// .then(() => {
-//     console.log('README successfully written!');
-// })
-// .catch((err) => { 
-//     console.log(err);
-// });
